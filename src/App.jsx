3d1fb1828a1c99fab7cc7793e2868de2746b6cc3
sotakeiteasy <murasov96@gmail.com>
2025-02-24 00:46:38 +0300

    First version

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+import './App.css'
+import CardContainer from './components/CardContainer/CardContainer'
+
+function App() {
+  return (
+    <>
+      <CardContainer/>
+    </>
+  )
+}
+
+export default App
